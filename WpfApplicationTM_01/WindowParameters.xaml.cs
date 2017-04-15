ea892939a1712d2d36400d079952a346f129f1d9
Minghao Xu <violiniselegant@gmail.com>
2017-04-15 12:27:41 +0800

    Add 2 windows

---
+++ WpfApplicationTM_01/WindowParameters.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplicationTM_01
+{
+    /// <summary>
+    /// WindowParameters.xaml 的交互逻辑
+    /// </summary>
+    public partial class WindowParameters : Window
+    {
+        public WindowParameters()
+        {
+            InitializeComponent();
+        }
+    }
+}
