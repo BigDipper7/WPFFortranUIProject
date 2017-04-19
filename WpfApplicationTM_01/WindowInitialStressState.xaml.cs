a4ff012f2039aee6e92ef07ffd4c8caed1d38cec
Minghao Xu <violiniselegant@gmail.com>
2017-04-19 15:18:48 +0800

    写文件操作

--- WpfApplicationTM_01/WindowInitialStressState.xaml.cs
+++ WpfApplicationTM_01/WindowInitialStressState.xaml.cs
@@ -30,7 +30,16 @@ namespace WpfApplicationTM_01
 
         private void btnConfirm_Click(object sender, RoutedEventArgs e)
         {
-            FileUtil.editFileContentByRow("test.txt", 2, "哈哈哈");
+            //FileUtil.editFileContentByRow("test.txt", 2, "哈哈哈");
+            string txt11 = this.txtBox11.Text;
+            string txt22 = this.txtBox22.Text;
+            string txt33 = this.txtBox33.Text;
+            string txt12 = this.txtBox12.Text;
+            string txt13 = this.txtBox13.Text;
+            string txt23 = this.txtBox23.Text;
+
+            string row10 = String.Format("{0} {1} {2} {3} {4} {5}", txt11, txt22, txt33, txt12, txt13, txt23);
+            FileUtil.editFileContentByRow("INPUT_simsand.txt", 10, row10);
             MessageBox.Show("succeeded");
             this.Close();
         }
